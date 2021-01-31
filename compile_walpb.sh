50e0041b2220f41d878d925f60ce0c154ab5027b
amazingchow <jianzhou42@163.com>
2021-01-31 17:38:29 +0800

    update compile_walpb.sh

--- compile_walpb.sh
+++ compile_walpb.sh
@@ -9,5 +9,5 @@ cd ${GOMODULEPATH}
 for i in $(ls ${GOMODULEPATH}/github.com/amazingchow/photon-dance-wal/walpb/*.proto); do
 	fn=github.com/amazingchow/photon-dance-wal/walpb/$(basename "$i")
 	echo "compile" $fn
-	/usr/local/bin/protoc --go_out=plugins=grpc:. "$fn"
+	/usr/local/bin/protoc --go_out=. "$fn"
 done
